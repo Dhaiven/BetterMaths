c9bfce63229c952cfaa00d27579dae901ff05d1e
Hugo_ <55756021+Dhaiven@users.noreply.github.com>
2024-03-26 17:35:07 +0100

    Add exp + refactoring

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-class EquaDiff:
-    def __init__(self, yPrime, unknow) -> None:
-        self.yPrime: str = yPrime
-        self.unknow = unknow
+import math
+
+class Equation:
+    def __init__(self, equation):
+        self.equation = self.toGoodEquation(equation)
     
     def toGoodEquation(self, equation: str):
         replacables = {
@@ -9,6 +10,7 @@ class EquaDiff:
             "+-": "-",
             "--": "-",
             "--": "+",
+            "^": "**",
         }
         for nbr in range(0, 10):
             replacables[str(nbr) + "("] = str(nbr) + "*("
@@ -16,9 +18,9 @@ class EquaDiff:
         for froms, to in replacables.items():
             equation = equation.replace(froms, to)
         return equation
-
-    def equation(self, equation: str):
-        equation = self.toGoodEquation(equation)
+    
+    def result(self):
+        equation = self.equation
         result = 0
         if "(" in equation:
             start = len(equation)
@@ -26,16 +28,37 @@ class EquaDiff:
             for i in range(len(equation)):
                 if equation[i] == "(" and i < start:
                     start = i
-                if equation[i] == ")" and i > end:
+                elif equation[i] == ")" and i > end:
                     end = i
             value = ""
             for i in range(start + 1, end):
                 value += equation[i]
-            return self.equation(equation[:start] + str(self.equation(value)) + equation[end + 1:])
+            functions = {
+                "exp": math.exp
+            }
+            for key, func in functions.items():
+                areFunction = True
+                for i in range(start - len(key) + 1, start + 1):
+                    if equation[start - i] != key[start - i]:
+                        areFunction = False
+                if areFunction:
+                    value = str(func(Equation(value).result()))
+                    start -= len(key)
+                    break
+
+            return Equation(equation[:start] + str(Equation(value).result()) + equation[end + 1:]).result()
         else:
             result += float(eval(equation))
         return result
     
+    def __str__(self) -> str:
+        return self.equation
+
+
+class EquaDiff:
+    def __init__(self, yPrime, unknow) -> None:
+        self.yPrime: str = yPrime
+        self.unknow = unknow
     def result(self, value = ""):
         if type(value) != str:
             value = str(value)
@@ -48,5 +71,7 @@ class EquaDiff:
         b = self.yPrime.split("+")[1]
         return self.toGoodEquation("Cexp(" + str(a) + self.unknow + ") + " + str(-int(b) / int(a)))
 
-diff = EquaDiff("2y+2", "y")
-print(diff.equation("2*7(1+-2)"))
+diff = Equation("exp(27)")
+print(diff.result())
+print(diff)
+print(eval("2**3"))
