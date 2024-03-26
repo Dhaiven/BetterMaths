af758e2df7b608c734ccdb64c6639cdac2199869
Hugo_ <55756021+Dhaiven@users.noreply.github.com>
2024-03-26 16:35:43 +0100

    Fix bugs + refactoring

--- main.py
+++ main.py
@@ -3,39 +3,19 @@ class EquaDiff:
         self.yPrime: str = yPrime
         self.unknow = unknow
     
-    def sum(self, equation: str):
-        values = equation.split("+", 1)
-        return self.equation(values[0]) + self.equation(values[1])
-    
-    def soustraction(self, equation: str):
-        values = equation.split("-", 1)
-        if values[0] == "":
-            v = values[1].split("-", 1)
-            if len(v) == 1:
-                return -float(values[1])
-            return -self.equation(v[0]) - self.equation(v[1])
-        return self.equation(values[0]) + self.equation("-" + values[1])
-
-    def multiplie(self, equation: str):
-        values = equation.split("*", 1)
-        return self.equation(values[0]) * self.equation(values[1])
-    
-    def divide(self, equation: str):
-        values = equation.split("/", 1)
-        return self.equation(values[0]) / self.equation(values[1])
-    
     def toGoodEquation(self, equation: str):
-        i = 0
-        good = ""
-        while i < len(equation):
-            element = equation[i]
-            if i < len(equation) - 1 and element + equation[i + 1] == "--":
-                good += "+"
-                i += 1
-            else:
-                good += element
-            i += 1
-        return good
+        replacables = {
+            "++": "+",
+            "+-": "-",
+            "--": "-",
+            "--": "+",
+        }
+        for nbr in range(0, 10):
+            replacables[str(nbr) + "("] = str(nbr) + "*("
+        
+        for froms, to in replacables.items():
+            equation = equation.replace(froms, to)
+        return equation
 
     def equation(self, equation: str):
         equation = self.toGoodEquation(equation)
@@ -52,16 +32,8 @@ class EquaDiff:
             for i in range(start + 1, end):
                 value += equation[i]
             return self.equation(equation[:start] + str(self.equation(value)) + equation[end + 1:])
-        if "+" in equation:
-            result += self.sum(equation)
-        elif "-" in equation:
-            result += self.soustraction(equation)
-        elif "*" in equation:
-            result += self.multiplie(equation)
-        elif "/" in equation:
-            result += self.divide(equation)
         else:
-            result += float(equation)
+            result += float(eval(equation))
         return result
     
     def result(self, value = ""):
@@ -72,10 +44,9 @@ class EquaDiff:
         #equation = self.yPrime.replace(self.unknow, str(value))
         a = self.yPrime.split("+")[0]
         a = a.replace(self.unknow, value)
-        a = self.multiplie(a)
+        a = self.equation(a)
         b = self.yPrime.split("+")[1]
-        return "Cexp(" + str(a) + self.unknow + ") + " + str(-int(b) / int(a))
+        return self.toGoodEquation("Cexp(" + str(a) + self.unknow + ") + " + str(-int(b) / int(a)))
 
 diff = EquaDiff("2y+2", "y")
-print(diff.equation("-2--7"))
-print(diff.equation("2*7(1+2)"))
+print(diff.equation("2*7(1+-2)"))
