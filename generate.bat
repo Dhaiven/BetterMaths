514a697db122e855a84f2419c9d8fbe90bf79d93
Dhaiven <hdhugo2006@outlook.com>
2024-03-31 16:15:37 +0200

    Fix generate.bat

--- generate.bat
+++ generate.bat
@@ -1,12 +1,14 @@
 @echo off
 
-python3 -version >nul 2>&1 && (
-  start python3 -m build
+python -version >nul 2>&1 && (
+  start python -m build
 ) || (
   start python3.12 -m build
+  pause
 )
 
 if %errorlevel%==0 echo line=%*
 
 echo "y" | [pip uninstall BetterMaths]
 start pip install dist\bettermaths-0.0.2-py3-none-any.whl
+pause
