12e298c925677fc95aacf44a6bdd36a3b9114598
algorythmTTV <loic.chauveau.kson@gmail.com>
2024-03-31 23:03:33 +0200

    added confidence, expected value, variance and deviation

--- src/BetterMaths/probability/main.py
+++ src/BetterMaths/probability/main.py
@@ -1,6 +1,10 @@
 from BetterMaths.main import Equation
 import math
 
+class Event:
+    def __init__(self, probability) -> None:
+        self.probability = probability
+
 class Binomial:
     """
     Represents a binomial distribution.
@@ -25,7 +29,7 @@ class Binomial:
             float: The probability of getting exactly k successes.
         """
         coeficient = math.factorial(self.n) / (math.factorial(k) * math.factorial(self.n - k))
-        return coeficient * self.p ** k * (1 - self.p) ** (self.n - k)
+        return coeficient * (self.p ** k) * ((1 - self.p) ** (self.n - k))
     
     def toEquation(self, k):
         """
@@ -41,7 +45,58 @@ class Binomial:
         p = str(self.p)
         return Equation(f"({n}!)/({k}!*({n}-{k})!)*{p}**{k}*(1-{p})**({n}-{k})")
 
+    def expectedValue(self):
+        """
+        Calculates the expected value of the binomial distribution.
+
+        Returns:
+            float: The expected value of the binomial distribution.
+        """
+        return self.n * self.p
+    
+    def variance(self):
+        """
+        Calculates the variance of the binomial distribution.
+
+        Returns:
+            float: The variance of the binomial distribution.
+        """
+        return self.expectedValue() * (1 - self.p)
+    
+    def standardDeviation(self):
+        """
+        Calculates the standard variation of the binomial distribution.
+
+        Returns:
+            float: The standard variation of the binomial distribution.
+        """
+        return math.sqrt(self.variance())
+    
+    def confidenceInterval(self,confidence:float) -> tuple:
+        """
+        Calculates the interval for the confidence.
+
+        Returns:
+            tuple: The confidence interval (a,b).
+        """
+        if not 0<=confidence<=100:
+            raise ValueError("The confidence level must be between 0 and 100%")
+        under=0
+        under_total=0
+        while under_total <= ((100-confidence)/2)/100:
+            under_total+=self.result(under)
+            under+=1
+            if under>self.n:
+                raise ValueError("Confidence too high!")
+        top=under-1
+        top_total=under_total
+        while top_total<(confidence+((100-confidence)/2))/100 and top<=self.n:
+            print(top,top_total)
+            top_total+=self.result(top)
+            top+=1
+        return (under-1, top)
 
 __all__ = [
+  "Event",
   "Binomial"
 ]
