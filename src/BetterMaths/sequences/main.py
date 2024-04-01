a97b76ee00c2166e7c2144aa5c4487838606d4e6
algorythmTTV <loic.chauveau.kson@gmail.com>
2024-04-01 11:31:20 +0200

    added things

--- src/BetterMaths/sequences/main.py
+++ src/BetterMaths/sequences/main.py
@@ -3,13 +3,46 @@ class Sequence:
         self.initial_term = initial_term
         self.expression = expression
     
-    def sequence(self, n) -> "list[int]":
+    def isArithmetic(self):
+        """
+        Checks if the sequence is arithmetic.
+
+        Returns:
+            If false:
+                bool
+            If true:
+                tuple: (True, difference)
+        """
+        sequence = self.sequence(2)
+        difference = sequence[1] - sequence[0]
+        for i in range(1, len(sequence)):
+            if sequence[i] - sequence[i - 1] != difference:
+                return False
+        return True
+    
+    def isGeometric(self):
+        """
+        Checks if the sequence is geometric.
+
+        Returns:
+            If false:
+                bool
+            If true:
+                tuple: (True, ratio)
+        """
+        sequence = self.sequence(2)
+        ratio = sequence[1] / sequence[0]
+        for i in range(1, len(sequence)):
+            if sequence[i] / sequence[i - 1] != ratio:
+                return False
+        return (True,ratio)
+    
+    def sequence(self,n) -> "list[int]":
         """
         Return the values of the n first terms of the sequence.
 
         Args:
-            expression (str): The expression defining the sequence.
-            n (int): The number of terms to generate.
+            n (int): The start of the sequence (default 0).
 
         Returns:
             list: The custom sequence as a list of integers.
@@ -20,14 +53,15 @@ class Sequence:
             sequence.append(term)
         return sequence
     
-    def variation(self) -> str:
+    def variation(self,n=0) -> str:
         """
         Calculates the variation of the sequence. It can be deacreasing, increasing or constant.
+        Calculate from n to n+1000 (default 0).
 
         Returns:
             str: The variation of the sequence.
         """
-        sequence = self.sequence(1)
+        sequence = self.sequence(1000)
         variation = 'constant'
         for i in range(1, len(sequence)):
             if sequence[i] > sequence[i - 1]:
@@ -35,38 +69,8 @@ class Sequence:
             elif sequence[i] < sequence[i - 1]:
                 variation = 'decreasing'
         return variation
-    
-    def isArithmetic(self) -> bool:
-        """
-        Checks if the sequence is arithmetic.
-
-        Returns:
-            bool: True if the sequence is arithmetic, False otherwise.
-        """
-        sequence = self.sequence(2)
-        difference = sequence[1] - sequence[0]
-        for i in range(1, len(sequence)):
-            if sequence[i] - sequence[i - 1] != difference:
-                return False
-        return True
-    
-    def isGeometric(self) -> bool:
-        """
-        Checks if the sequence is geometric.
-
-        Returns:
-            bool: True if the sequence is geometric, False otherwise.
-        """
-        sequence = self.sequence(2)
-        ratio = sequence[1] / sequence[0]
-        for i in range(1, len(sequence)):
-            if sequence[i] / sequence[i - 1] != ratio:
-                return False
-        return True
         
 
-caca=Sequence(23, "(n+2)*2")
-print(caca.sequence(5))
-print(caca.variation())
+caca=Sequence(23, "n*2")
 print(caca.isArithmetic())
 print(caca.isGeometric())
